bcdd6ae3c77aac94b9abf0840884d5ebd4b13afa
Prajwal P B <prajwalpb8055@gmail.com>
2026-01-30 10:11:27 +0530

    4

---
+++ move_zeroes/move.java
@@ -0,0 +1,25 @@
+public class move {
+
+    public static void moveZeroes(int[] nums){
+        int lastNonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroIndex] = nums[i];
+                lastNonZeroIndex++;
+            }
+        }
+
+        for (int i = lastNonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }    
+}
